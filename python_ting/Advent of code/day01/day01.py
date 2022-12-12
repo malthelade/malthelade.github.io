ce991fb87d391ac2e277e4365ab389ec1f360fe3
malthelade <ladefogedmalthe@gmail.com>
2022-12-12 14:21:34 +0100

    day01 aoc

---
+++ python_ting/Advent of code/day01/day01.py
@@ -0,0 +1,36 @@
+def solutionA(lines):
+    max_calories = 0
+    calories = 0
+    for line in lines:
+        if not line:
+            calories = 0
+        else:
+            calories += int(line)
+            if calories > max_calories:
+                max_calories = calories
+    
+    return max_calories
+
+
+
+
+
+# Helper function for loading the problem data
+def load_data(fileName):
+  with open(fileName, "r") as input_data:
+    lines = input_data.readlines()
+  for i in range(len(lines)):
+    lines[i] = lines[i].strip()
+  return lines
+
+
+if __name__ == "__main__":
+  # input_file_name = "dummy-input.txt"
+  # TODO: Uncomment line below to use real input
+  input_file_name = "input.txt" 
+  
+  print("Loading data")
+  lines = load_data(input_file_name)
+  
+  resultA = solutionA(lines)
+  print(f"Solution for part A: {resultA}")
